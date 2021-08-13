44751586aee3ab942c0b457c45b0f97ed0abe818
YunYoonBin <yubiny287@gmail.com>
2021-08-13 22:46:24 +0900

    로또

---
+++ 로또/src/project/로또.java
@@ -0,0 +1,27 @@
+package project;
+
+public class 로또 {
+	public static void main(String[] args) {
+		      //변수 생성
+		int lotto[] = new int [6];
+		
+     	  	// 번호 생성
+		for(int i=0; i<6; i++) {
+			lotto[i] = (int)(Math.random() * 45) + 1;
+            
+       		  	 // 중복 번호 제거
+			for(int j=0; j<i; j++) {
+				if(lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+			}
+		}
+	System.out.print("로또 번호: ");
+	
+  	 // 번호 출력
+	for(int i=0; i<6; i++) {
+		System.out.print(lotto[i] + " ");
+	}	
+	}
+}
